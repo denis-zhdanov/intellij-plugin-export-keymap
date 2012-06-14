58fa069527e0c6d8656b26ab57df103f409657f2
Denis.Zhdanov <Denis.Zhdanov@jetbrains.com>
2012-06-14 12:44:31 +0400

    Groovify the code

--- src/main/java/org/denis/intellij/export/keymap/generator/Generator.groovy
+++ src/main/java/org/denis/intellij/export/keymap/generator/Generator.groovy
@@ -1,7 +1,6 @@
 package org.denis.intellij.export.keymap.generator
 
 import com.itextpdf.text.pdf.PdfPCell
-import com.itextpdf.text.pdf.PdfPRow
 import com.itextpdf.text.pdf.PdfPTable
 import com.itextpdf.text.pdf.PdfWriter
 import org.denis.intellij.export.keymap.Bundle
@@ -9,7 +8,6 @@ import org.jetbrains.annotations.NotNull
 import com.itextpdf.text.*
 
 import static org.denis.intellij.export.keymap.generator.GenerationConstants.*
-import com.intellij.openapi.util.Pair
 
 /**
  * @author Denis Zhdanov
@@ -34,44 +32,35 @@ class Generator {
     document.addSubject(Bundle.message("document.subject", context.keymapName))
     document.open()
 
-    PdfPTable rootTable = new PdfPTable(COLUMNS_NUMBER);
-    rootTable.setWidthPercentage(100);
-    for (i in 0..<COLUMNS_NUMBER) {
-      rootTable.addCell(new PdfPCell(paddingLeft: 0f, paddingRight: 20f, border: Rectangle.NO_BORDER))
-    }
+    PdfPTable rootTable = new PdfPTable(COLUMNS_NUMBER)
+    rootTable.setWidthPercentage(100)
+    COLUMNS_NUMBER.times { rootTable.addCell(new PdfPCell(paddingLeft: 0f, paddingRight: 20f, border: Rectangle.NO_BORDER)) }
     rootTable.getRow(0).getCells().each { context.columns << it }
 
-    addData(context);
+    addData(context)
 
-    PdfPTable headerTable = new PdfPTable(1);
-    headerTable.widthPercentage = 100;
-    def headerFont = new Font(FONT_FAMILY, HEADER_FONT_SIZE, Font.BOLD, TITLE_COLOR);
-    final PdfPCell headerCell = new PdfPCell(new Paragraph(Bundle.message("document.header", context.keymapName), headerFont));
-    headerCell.border = Rectangle.NO_BORDER;
-    headerTable.addCell(headerCell);
-    document.add(headerTable);
+    PdfPTable headerTable = new PdfPTable(1)
+    headerTable.widthPercentage = 100
+    def headerFont = new Font(FONT_FAMILY, HEADER_FONT_SIZE, Font.BOLD, TITLE_COLOR)
+    def headerCell = new PdfPCell(new Paragraph(Bundle.message("document.header", context.keymapName), headerFont))
+    headerCell.border = Rectangle.NO_BORDER
+    headerTable.addCell(headerCell)
+    document.add(headerTable)
 
-    document.add(rootTable);
-    document.close();
+    document.add(rootTable)
+    document.close()
 
     // Calculate real row heights. iText doesn't provide an API to do it without flushing the document.
     if (!context.realGenerationIteration) {
-      for (element in rootTable.getRow(0).cells[0].compositeElements) {
-        PdfPTable table = (PdfPTable)element;
-        for (PdfPRow row : table.getRows()) {
-          context.rowHeights << row.getCells()[0].getHeight()
-        }
-      }
+      rootTable.getRow(0).cells[0].compositeElements.rows*.each { context.rowHeights << it.getCells()[0].height }
       context.maxTableHeight = document.pageSize.height - headerTable.totalHeight - document.bottomMargin() - document.topMargin()
     }
   }
 
   private def addData(@NotNull GenerationContext context) {
     for (sectionData in context.data) {
-      PdfPTable table = addSection(sectionData.name, context)
-      for (Pair<String, String> pair : sectionData.actions) {
-        table = addRow(table, pair.first, pair.second, context)
-      }
+      def table = addSection(sectionData.name, context)
+      sectionData.actions.each { table = addRow(table, it.first, it.second, context) }
     }
   }
 
@@ -80,10 +69,10 @@ class Generator {
     result.setWidthPercentage(100)
     def font = new Font(FONT_FAMILY, ACTION_GROUP_FONT_SIZE, Font.BOLD, TITLE_COLOR)
     def titleCell = new PdfPCell(new Paragraph(title, font))
-    titleCell.setBorder(Rectangle.BOTTOM)
-    titleCell.setBorderColor(COLOR_BORDER_HEADER);
-    titleCell.setColspan(2);
-    result.addCell(titleCell);
+    titleCell.border = Rectangle.BOTTOM
+    titleCell.borderColor = COLOR_BORDER_HEADER
+    titleCell.colspan = 2
+    result.addCell(titleCell)
     
     context.updateColumn(2)
     context.currentColumn().addElement(result)
@@ -96,27 +85,26 @@ class Generator {
                                   @NotNull String value,
                                   @NotNull GenerationContext context)
   {
-    def tableToUse = table;
+    def tableToUse = table
     context.updateColumn(1, {
       tableToUse = new PdfPTable(2)
       tableToUse.widthPercentage = 100
       it.addElement(tableToUse)
     })
 
-    Font keyFont = new Font(FONT_FAMILY, DATA_FONT_SIZE, Font.BOLD)
-    PdfPCell keyCell = new PdfPCell(new Paragraph(key, keyFont))
-    keyCell.setBorder(Rectangle.BOTTOM)
-    keyCell.setBackgroundColor(COLOR_BACKGROUND_KEY)
-    keyCell.setBorderColor(COLOR_BORDER_DATA)
+    def keyFont = new Font(FONT_FAMILY, DATA_FONT_SIZE, Font.BOLD)
+    def keyCell = new PdfPCell(new Paragraph(key, keyFont))
+    keyCell.border = Rectangle.BOTTOM
+    keyCell.backgroundColor = COLOR_BACKGROUND_KEY
+    keyCell.borderColor = COLOR_BORDER_DATA
     tableToUse.addCell(keyCell)
 
-    Font valueFont = new Font(FONT_FAMILY, DATA_FONT_SIZE)
-    final PdfPCell valueCell = new PdfPCell(new Paragraph(value, valueFont))
-    valueCell.setBorder(Rectangle.BOTTOM)
-    valueCell.setBorderColor(COLOR_BORDER_DATA)
+    def valueFont = new Font(FONT_FAMILY, DATA_FONT_SIZE)
+    def valueCell = new PdfPCell(new Paragraph(value, valueFont))
+    valueCell.border = Rectangle.BOTTOM
+    valueCell.borderColor = COLOR_BORDER_DATA
     tableToUse.addCell(valueCell)
     context.onNewRow()
     tableToUse
   }
-
 }
